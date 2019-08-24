054bf1c59a9cfa792b3f5975eee30c5891f63532
Ubuntu <ubuntu@ip-172-31-88-174.ec2.internal>
2019-08-24 23:16:56 +0000

    updated urls.py file to include url, updated html, tidy code, include font awesome 5 icons. ensure search function works

--- search/views.py
+++ search/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from bug.models import Bug
+from ticket.models import Ticket
 
 # Create your views here.
+def search(request):
+    bug = Bug.objects.filter(title__icontains=request.GET['q'])
+    ticket = Ticket.objects.filter(title__icontains=request.GET['q'])
+    bug_count = bug.count()
+    ticket_count = ticket.count()
+    
+    context = {
+        'bug': bug,
+        'ticket': ticket,
+        'bug_count': bug_count,
+        'ticket_count': ticket_count,
+    }
+    return render(request, "search.html", context)
